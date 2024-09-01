06039899fd5de395cf53a645f1b362246df9a891
FranciscoAlva <FALVAREZR91@alumnos.imf.com>
2024-09-01 19:50:02 +0200

    CLASES CRTL_USUARIO Y USUARIO
    EN EL LA CARPETA LIBRERIAS AGREGAMOS EL CONECTOR DE MYSQL PARA CONECTARNOS A LA BASE DE DATOS.
    
    CREAMOS EN EL PAQUETE CONTROLADOR LA CLASE CTRLUSUARIO Y EN EL PAQUETE MODELO LA CLASE USUARIO.
    
    EN LA CLASE USUARIO GENERAMOS LOS GETTER AND SETTER
    
    EN LA CLASE CTRLUSUARIO CREAMOS EL METODO LOGINUSER E IMPORTAMOS LAS LIBRERIAS
    import conexion.Conexion;
    import java.sql.Connection;
    import modelo.Usuario;
    import java.sql.Statement;
    import javax.swing.JOptionPane;
    import java.sql.SQLException;
    import java.sql.ResultSet;
    
    LUEGO CREAMOS TODO EL CODIGO PARA LA CONEXION DONDE USAMOS UN TRY/CATCH Y DENTRO DLE EL UN WHILE YA QUE SI SE EJECUTA DE MANERA CORRECTAMENTE NO SIGA EL CODIGO Y ENTRE EN EL SISTEMA .

---
+++ SistemaDeVentas1/src/controlador/Ctrl_Usuario.java
@@ -0,0 +1,39 @@
+package controlador;
+
+import conexion.Conexion;
+import java.sql.Connection;
+import modelo.Usuario;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author maygr
+ */
+public class Ctrl_Usuario {
+
+    //METODO PARA INICIAR SESION
+    public boolean loginUser(Usuario objeto) {
+
+        boolean respuesta = false;
+        Connection cn = Conexion.conectar();
+        String sql = "SELECT * FROM usuarios WHERE usuario = '" + objeto.getUsuario() + "' AND password = '" + objeto.getPassword() + "'";
+        Statement st;
+        try {
+            st = cn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            while (rs.next()) {
+                respuesta = true;
+            }
+        } catch (SQLException e) {
+            System.out.println("ERROR AL INICIAR SESION");
+            JOptionPane.showMessageDialog(null, "ERROR AL INICIAR SESION");
+        }
+        
+        return respuesta;
+
+    }
+
+}
---
+++ SistemaDeVentas1/src/modelo/Usuario.java
@@ -0,0 +1,117 @@
+package modelo;
+
+/**
+ *
+ * @author maygr
+ */
+public class Usuario {
+    
+    //ATRIBUTOS
+    private int idUsuario;
+    private String dni;
+    private String nombre;
+    private String apellidos;
+    private String telefono;
+    private String email;
+    private String direccion;
+    private String password;
+    private String usuario;
+    private int estado;
+    
+    //CONSTRUCTOR
+    public Usuario (){
+        this.idUsuario = 0;
+        this.dni = "";
+        this.nombre = "";
+        this.apellidos = "";
+        this.telefono = "";
+        this.email = "";
+        this.direccion = "";
+        this.password = "";
+        this.usuario = "";
+        this.estado = 0;        
+    }
+    
+    //SETTER AND GETTER
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }    
+
+}
